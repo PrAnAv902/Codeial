9322c3ca2f228c77aaf185ac69bfe993f7b36b29
Pranav Chaudhary <pranavchaudhary69@gmail.com>
2022-06-13 17:05:19 +0530

    add a route and controller for profile route

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,5 @@ const homeController=require('../controllers/home_controller');
 console.log('route loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
 module.exports = router;
