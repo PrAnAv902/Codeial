ea72c92baaa126f9197b15f88c611870673a33d1
Pranav Chaudhary <pranavchaudhary69@gmail.com>
2022-06-24 15:54:27 +0530

    Tried and implemented manual auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,20 @@
+const { resolveInclude } = require('ejs');
 const User=require('../models/user');
 module.exports.profile = function(req,res){
-    return res.render('users',{
-        title:"PROFILES"
+   if(req.cookies.user_id){
+    User.findById(req.cookies.user_id,function(err,user){
+        if(user){
+            return res.render('users',{
+                title:"User Profile",
+                user: user
+            });
+        }
     });
+   }else{
+     return res.redirect('/users/sign-in');
+   }
+   
+
 }
 //render the sign up page
 module.exports.signUp=function(req,res){
@@ -45,5 +57,21 @@ module.exports.create=function(req,res){
 }
 //get the sign in data and create a session for user
 module.exports.createSession=function(req,res){
+User.findOne({email:req.body.email},function(err,user){
+    if(err){
+        console.log('Error in finding user in signing in');
+        return;
+    }
+    if(user){
+        if(user.password!=req.body.password){
+            return res.redirect('back');
+        }
 
+        res.cookie('user_id', user.id);
+        return res.redirect('/users/profile');
+    }
+    else{
+        return res.redirect('/users/sign-up');
+        }
+})
 }
